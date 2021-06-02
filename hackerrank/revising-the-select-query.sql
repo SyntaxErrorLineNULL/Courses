b46a3aff0e48a850b1f96616a44259b4cbc93f22
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 23:12:07 +0300

    create hackerrank sql problem solving, create table city

---
+++ hackerrank/revising-the-select-query.sql
@@ -0,0 +1,11 @@
+/**
+  Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+ */
+
+CREATE TABLE CITY (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    NAME VARCHAR(17),
+    COUNTRYCODE VARCHAR(3),
+    DISTRICT VARCHAR(20),
+    POPULATION INT
+);
