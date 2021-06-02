5697841c950d1fe1c5a64f29be5ccb0d16e0491f
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 13:49:09 +0300

    sql script insert data from table book

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -102,3 +102,13 @@ CREATE TABLE book (
     FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE CASCADE,
     FOREIGN KEY (genre_id) REFERENCES genre (genre_id) ON DELETE SET NULL
 );
+
+INSERT INTO book (title, author_id, genre_id, price, amount) VALUES
+    ('Мастер и Маргарита', 1, 1, 670.99, 3),
+    ('Белая гвардия ', 1, 1, 540.50, 5),
+    ('Идиот', 2, 1, 460.00, 10),
+    ('Братья Карамазовы', 2, 1, 799.01, 3),
+    ('Игрок', 2, 1, 480.50, 10),
+    ('Стихотворения и поэмы', 3, 2, 650.00, 15),
+    ('Черный человек', 3, 2, 570.20, 6),
+    ('Лирика', 4, 2, 518.99, 2);
