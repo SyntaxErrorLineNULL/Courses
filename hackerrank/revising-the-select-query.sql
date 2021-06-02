d8aa97a47763986137f209ef35357deec98c5fe0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 23:41:36 +0300

    solving Weather Observation Station 3

--- hackerrank/revising-the-select-query.sql
+++ hackerrank/revising-the-select-query.sql
@@ -55,6 +55,7 @@ SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
 
 /**
   Weather Observation Station 1
+  Query a list of CITY and STATE from the STATION table.
  */
 
 CREATE TABLE STATION (
@@ -66,3 +67,11 @@ CREATE TABLE STATION (
 );
 
 SELECT CITY, STATE FROM STATION;
+
+/**
+  Weather Observation Station 3
+  Query a list of CITY names from STATION for cities that have an even ID number.
+  Print the results in any order, but exclude duplicates from the answer.
+ */
+
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0;
