0ddf29be726544df549f6e752286419c534ef5cb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 13:31:27 +0300

    solving 2.3

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -21,9 +21,24 @@ INSERT INTO author (author_id, name_author) VALUES
 
 /**
   2.3
+  Перепишите запрос на создание таблицы book , чтобы ее структура соответствовала структуре, показанной на логической схеме
+  (таблица genre уже создана, порядок следования столбцов - как на логической схеме в таблице book, genre_id  - внешний ключ) .
+  Для genre_id ограничение о недопустимости пустых значений не задавать. В качестве главной таблицы для описания поля  genre_id
  */
 
 CREATE TABLE genre (
     genre_id INT PRIMARY KEY AUTO_INCREMENT,
     name_genre VARCHAR(50)
 );
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8,2),
+    amount INT,
+    FOREIGN KEY (author_id)  REFERENCES author (author_id),
+    FOREIGN KEY (genre_id)  REFERENCES genre (genre_id)
+);
+
