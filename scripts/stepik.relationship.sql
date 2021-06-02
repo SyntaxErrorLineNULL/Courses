d988ed3d422e220c72c1757be1a9dd26cc084535
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 13:43:52 +0300

    solving 2.5

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -59,3 +59,12 @@ CREATE TABLE book (
     FOREIGN KEY (author_id)  REFERENCES author (author_id) ON DELETE CASCADE,
     FOREIGN KEY (genre_id)  REFERENCES genre (genre_id) ON DELETE SET NULL
 );
+
+/**
+  2.5
+ */
+
+INSERT INTO book (book_id, title, author_id, genre_id, price, amount) VALUES
+    (6, 'Стихотворения и поэмы', 3, 2, 650.00, 15),
+    (7, 'Черный человек', 3, 2, 570.20, 6),
+    (8, 'Лирика', 4, 2, 518.99, 2);
