4f4794cc9dd459cf2d14abed50399b3fd4624fdf
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 15:21:28 +0300

    solving 2.2.3, add example query

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -185,3 +185,17 @@ FROM author
     INNER JOIN  book ON author.author_id = book.author_id
     INNER JOIN genre ON genre.genre_id = book.genre_id
 WHERE price BETWEEN 500 AND 700;
+
+/**
+  2.2.4
+  Вывести информацию о книгах (жанр, книга, автор), относящихся к жанру, включающему слово «роман» в отсортированном по названиям книг виде.
+ */
+
+SELECT name_genre, title, name_author
+FROM book
+    INNER JOIN author
+            ON book.author_id = author.author_id
+    INNER JOIN genre
+            ON book.genre_id = genre.genre_id
+WHERE name_genre = 'Роман'
+ORDER BY title;
