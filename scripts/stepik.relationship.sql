136e484aba9123428e44d4963cd80eb5394bcccf
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 13:45:49 +0300

    sql script create table author

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -68,3 +68,13 @@ INSERT INTO book (book_id, title, author_id, genre_id, price, amount) VALUES
     (6, 'Стихотворения и поэмы', 3, 2, 650.00, 15),
     (7, 'Черный человек', 3, 2, 570.20, 6),
     (8, 'Лирика', 4, 2, 518.99, 2);
+
+/** DATABASE */
+
+CREATE TABLE author(
+    author_id serial PRIMARY KEY,
+    name_author VARCHAR(50)
+);
+
+
+
