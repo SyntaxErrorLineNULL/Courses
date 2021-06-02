6db564aaf47a5222e356727c00fad4c07d9b9f27
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 13:22:29 +0300

    solving 2.2

--- scripts/stepik.relationship.sql
+++ scripts/stepik.relationship.sql
@@ -7,3 +7,14 @@ CREATE TABLE author (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     name_author VARCHAR(50)
 );
+
+/**
+  2.2
+  Заполнить таблицу author.
+ */
+
+INSERT INTO author (author_id, name_author) VALUES
+    (1, 'Булгаков М.А.'),
+    (2, 'Достоевский Ф.М.'),
+    (3, 'Есенин С.А.'),
+    (4, 'Пастернак Б.Л.');
