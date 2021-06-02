a136e7eb08d5e577b368b4955a9abb702662994e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-06-02 23:18:29 +0300

    insert test data

--- hackerrank/revising-the-select-query.sql
+++ hackerrank/revising-the-select-query.sql
@@ -10,4 +10,15 @@ CREATE TABLE CITY (
     POPULATION INT
 );
 
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES
+    (6, 'Rotterdam', 'NLD', 'Zuid-Holland', 593321),
+    (3878, 'Scottsdale', 'USA', 'Arizona', 202705),
+    (3965, 'Corona', 'USA', 'California', 124966),
+    (3973, 'Concord', 'USA', 'California', 121780),
+    (3977, 'Cedar Rapids', 'USA', 'Iowa', 120758),
+    (3982, 'Coral Springs', 'USA', 'Florida', 117549),
+    (4054, 'Fairfield', 'USA', 'California', 92256),
+    (4058, 'Boulder', 'USA', 'California', 91238),
+    (4061, 'Fall River', 'USA', 'Massachusetts', 90555);
+
 SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
